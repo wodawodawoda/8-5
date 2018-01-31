329d9b2354ff1575ecf6184bd4178d663e59b73b
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-01-31 22:03:51 +0100

    Add script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var a = prompt('Set length'),
+	h = prompt('Set height'),
+	triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is rqual to: ' + triangleArea);
